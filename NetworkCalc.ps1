767f35a535edfacfa0ab8e5d115eadb63be669f1
hugo-paulo <47899992+hugo-paulo@users.noreply.github.com>
2023-12-05 18:41:33 +0000

    Add files via upload
    Script for calculating network hosts and subnets

---
+++ NetworkCalc.ps1
@@ -0,0 +1,41 @@
+﻿$inputAddress = Read-Host "Provide Network address"
+$AddressFormat = $inputAddress.Split(".") 
+
+$1stOct = $AddressFormat[0]
+$2ndOct = $AddressFormat[1]
+$3rdOct = $AddressFormat[2]
+
+$4thOct = $AddressFormat[3].Split("/")[0]
+$subnetMask = $AddressFormat[3].Split("/")[1]
+
+$AddressSize = 32
+
+$numberOfHosts = [Math]::Pow(2,($AddressSize - $subnetMask))
+$numberOfUsableHosts = $numberOfHosts - 2
+
+Write-Host "The numer of hosts: $numberOfHosts"
+Write-Host "The numer of usable hosts available: $numberOfUsableHosts"
+
+$classA = $1stOct -In 1 ...126 
+$classB = $1stOct -In 128 ...191
+$classC = $1stOct -In 192 ...223
+
+if($classA)
+{
+   $numberOfSubNets =  [Math]::Pow(2,($subnetMask - 24))
+   Write-Host "The numer of subnets: $numberOfSubNets"
+}
+elseif($classB)
+{
+   $numberOfSubNets =  [Math]::Pow(2,($subnetMask - 16))
+   Write-Host "The numer of subnets: $numberOfSubNets"
+}
+elseif($classC)
+{
+   $numberOfSubNets =  [Math]::Pow(2,($subnetMask - 8))
+   Write-Host "The numer of subnets: $numberOfSubNets"
+}
+else
+{
+   Write-Host "The network address provided: $inputAddress should not be used" -BackgroundColor Yellow
+}
